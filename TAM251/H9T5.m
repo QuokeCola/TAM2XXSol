b6c0c13fe5fe2cfa28b0f833ddceb8fff6dadaa6
QuokeCola <quokecola@icloud.com>
2020-04-18 22:52:15 +0800

    Halfdone TAM251 Homework9 T6

--- TAM251/H9T5.m
+++ TAM251/H9T5.m
@@ -19,11 +19,9 @@ syms x y
 
 Iz = vpa(int(int(y^2,x,-sqrt((do/2)^2-y^2),sqrt((do/2)^2-y^2)),y,-do/2,do/2)-int(int(y^2,x,-sqrt((di/2)^2-y^2),sqrt((di/2)^2-y^2)),y,-di/2,di/2));
 J = vpa(int(int(x^2+y^2,x,-sqrt((do/2)^2-y^2),sqrt((do/2)^2-y^2)),y,-do/2,do/2)-int(int(x^2+y^2,x,-sqrt((di/2)^2-y^2),sqrt((di/2)^2-y^2)),y,-di/2,di/2));
+Q = vpa(int(int(y,x,-sqrt((do/2)^2-y^2),sqrt((do/2)^2-y^2)),y,0,do/2)-int(int(y,x,-sqrt((di/2)^2-y^2),sqrt((di/2)^2-y^2)),y,0,di/2));
 
 SigmaX = My*do/2/Iz*1e3+p*pi*(di/2)^2*1e3/(pi/4*(do^2-di^2))/1e6
 
 SigmaY = p*di/2/(do-di)*2/1e3
-
-Q = vpa(int(int(y,x,-sqrt((do/2)^2-y^2),sqrt((do/2)^2-y^2)),y,0,do/2)-int(int(y,x,-sqrt((di/2)^2-y^2),sqrt((di/2)^2-y^2)),y,0,di/2));
-
 TauXY = -vpa(Tx*do/2/J*1e3+absVy*Q/Iz/(do-di))
--- TAM251/H9T6.m
+++ TAM251/H9T6.m
@@ -21,6 +21,6 @@ J = vpa(int(int(x^2+y^2,x,-sqrt((do/2)^2-y^2),sqrt((do/2)^2-y^2)),y,-do/2,do/2)-
 Q = vpa(int(int(y,x,-sqrt((do/2)^2-y^2),sqrt((do/2)^2-y^2)),y,0,do/2)-int(int(y,x,-sqrt((di/2)^2-y^2),sqrt((di/2)^2-y^2)),y,0,di/2));
 
 SigmaX = Mz*do/2/Iz*1e3
-SigmaZ = p*di/2/(do-di)*2/1e3
 
+SigmaZ = p*di/2/(do-di)*2/1e3
 TauXZ = vpa(Tx*do/2/J*1e3+absVz*Q/Iz/(do-di))
